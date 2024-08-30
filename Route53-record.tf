87371a0f489bdf034fa01bb072206a13c84a6ddb
Robert <roberttemta@gmail.com>
2024-08-30 10:28:07 -0500

    Route53

--- Route53-record.tf
+++ Route53-record.tf
@@ -9,11 +9,12 @@ resource "aws_route53_record" "site_domain" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = var.record_name
   type    = "A"
+ 
+  
 
   alias {
     name    = aws_lb.load-balancer.dns_name
     zone_id = aws_lb.load-balancer.zone_id
-    //zone_id                = var.application_load_balancer_zone_id
     evaluate_target_health = true
   }
 }
